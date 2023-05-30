e1928e8d41181f767e32c496fbbc7f5d1612a9cb
sowmyanadh123 <sowmyanadh123@gmail.com>
2023-05-30 14:56:17 +0530

    added test

--- src/test/java/com/nopCommers/login/logIn.java
+++ src/test/java/com/nopCommers/login/logIn.java
@@ -1,7 +1,21 @@
 package com.nopCommers.login;
 
-public class logIn {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
+public class logIn {
+ 
+  @org.testng.annotations.Test
+	public void Test() {
+	//System.setProperty("webdriver.chrome.driver","C:\\\\Testing\\\\drivers\\\\chromedriver.exe");
 	
+	WebDriverManager.chromedriver().setup();
 	
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://www.amazonlogin.com/");
+     driver.findElement(By.id("sowmyanadh"));
+     driver.close();
+	}
 }
